69190a7ce3816fac95fac9464a0ada149a38e216
Piyu2006 <akshatrk37@gmail.com>
2026-01-31 14:59:51 +0530

    first commit

---
+++ Internal Assginment/1_program.py
@@ -0,0 +1,6 @@
+def reverse_array(arr):
+    arr.reverse()
+    return arr
+
+arr = [1, 4, 3, 2, 6, 5]
+print("Reversed Array:", reverse_array(arr))
---
+++ Internal Assginment/2_program.py
@@ -0,0 +1,7 @@
+def find_min_max(arr):
+    return min(arr), max(arr)
+
+arr = [1, 4, 3, 5, 8, 6]
+mn, mx = find_min_max(arr)
+print("Minimum =", mn)
+print("Maximum =", mx)
